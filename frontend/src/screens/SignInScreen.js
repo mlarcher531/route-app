feb5a88cfdae6ac855c0488a486b3aa6b577151d
mlarcher531 <mlarcher531@gmail.com>
2021-01-20 15:44:58 -0600

    navigation

---
+++ frontend/src/screens/SignInScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const SignInScreen = () => {
+
+    return (
+        <View>
+            <Text>SignInScreen</Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({})
+
+export default SignInScreen
---
+++ frontend/src/screens/SignUpScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const SignUpScreen = () => {
+
+    return (
+        <View>
+            <Text>SignUpScreen</Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({})
+
+export default SignUpScreen
