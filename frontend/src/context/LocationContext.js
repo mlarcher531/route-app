d7c3fe0b267d641b2e513f8970141e9720557a9a
mlarcher531 <mlarcher531@gmail.com>
2021-01-21 12:24:23 -0600

    custom location hook

---
+++ frontend/src/context/LocationContext.js
@@ -0,0 +1,35 @@
+import createDataContext from './createDataContext'
+
+const locationReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_LOCATION':
+            return {...state, currentLocation: action.payload}
+        default:
+            return state
+    }
+}
+
+const startRecording = (dispatch) => {
+    return () =>{
+        
+    }
+}
+
+const stopRecording = (dispatch) => {
+    return () =>{
+        
+    }
+}
+
+const addLocation = (dispatch) => {
+    return (location) =>{
+        dispatch({ type: 'ADD_LOCATION', payload: location})
+    }
+}
+
+
+export const { Context, Provider } = createDataContext(
+    locationReducer,
+    {startRecording, stopRecording, addLocation },
+    {recording: false, locations: [], currentLocation: null}
+)
--- frontend/src/screens/TrackCreateScreen.js
+++ frontend/src/screens/TrackCreateScreen.js
@@ -1,42 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
 import Map from '../components/Map'
-import { requestPermissionsAsync } from 'expo-location'
+import { Context as LocationContext } from '../context/LocationContext'
+import useLocation from '../hooks/useLocation'
+
+// import '../_mockLocation'
 
 const TrackCreateScreen = () => {
 
-    const [err, setErr] = useState(null)
+    const { addLocation } = useContext(LocationContext)
 
-    
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestPermissionsAsync();
-            if (!granted) {
-                throw new Error('Location permission not granted');
-            }
-        } catch (error) {
-            setErr(error);
-        }
-    };
+    const [err] = useLocation(addLocation)
 
-    useEffect(()=>{
-        startWatching()
-    },[])
-    
     return (
-        <SafeAreaView forceInset={{top: 'always'}}>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h3>Create A Track</Text>
             <Map />
             {err ? <Text style={styles.error}>Please Enable Location Services</Text> : null}
         </SafeAreaView>
     )
-
 }
 
 const styles = StyleSheet.create({
-    error:{
+    error: {
         color: 'red',
         textAlign: 'center',
         fontSize: 15,
